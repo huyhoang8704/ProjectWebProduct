914208def24e8bf97e8056185979058ca5064d85
huyhoang8704 <aye10diemtoan0@gmail.com>
2024-05-25 02:23:33 +0700

    boostrap + file tĩnh

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 require("dotenv").config();
+const route = require('./routes/client/index.route')
+
 
 const app = express()
 const port = process.env.PORT ;
 
-const route = require('./routes/client/index.route')
-
 app.set('views', './views');
 app.set('view engine', 'pug');
+
+app.use(express.static("public"));
+
 
 //! Route
 route(app)
