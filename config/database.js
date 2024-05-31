089c3bfe376cb999332461826ff4b3ff37164294
huyhoang8704 <aye10diemtoan0@gmail.com>
2024-05-31 10:01:28 +0700

    connect mongodb online

--- config/database.js
+++ config/database.js
@@ -3,9 +3,16 @@ const mongoose = require('mongoose');
 
 module.exports.connect = async () => {
     try {
-       await mongoose.connect(process.env.MONGO_URL)
+    // 'mongodb+srv://huyhoang8704:0985769742a@cluster0.r3jnlvf.mongodb.net/ProductManagement'
+       await mongoose.connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverSelectionTimeoutMS: 30000, // Tăng thời gian chờ kết nối
+        socketTimeoutMS: 45000, // Tăng thời gian chờ socket
+      })
        console.log("Mongodb Connect Successfully")
     } catch (error) {
+        console.log(error)
         console.log("Mongodb Connect Failed")
     }
 }
