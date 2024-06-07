5161af92c6a7893b4172f770f849b5ecbe690142
huyhoang8704 <aye10diemtoan0@gmail.com>
2024-06-07 10:27:17 +0700

    connection Accounts

---
+++ models/accounts.model.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const generate = require("../helpers/generate")
+
+const Account_schema = new mongoose.Schema(
+    {
+        fullname: String,
+        email : String,
+        password : String,
+
+        // unique , not public
+        token :{
+            type: String,
+            default: generate.generateRandomString(20),
+        },
+
+        phone : String,
+        avatar : String,
+        rold_id : String,
+        status : String,
+
+
+        deleted: {
+            type : Boolean,
+            default: false,
+        },
+        deletedAt: Date,
+
+    },
+    {
+        timestamps : true
+    }
+);
+
+
+const Account = mongoose.model("Account",Account_schema,"accounts") // argu3 là tên connection trong db
+
+
+module.exports = Account;
