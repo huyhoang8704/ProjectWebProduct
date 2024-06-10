f052c8a6d437ea02996d94158f186ef7f19876ea
huyhoang8704 <aye10diemtoan0@gmail.com>
2024-06-11 05:55:34 +0700

    Thêm authorization

--- middlewares/admin/auth.middleware.js
+++ middlewares/admin/auth.middleware.js
@@ -10,10 +10,19 @@ module.exports.requireAuth = async (req , res, next) => {
         res.redirect(`${systemConfig.prefixAdmin}/auth/login`)
     } else {
         // console.log("Token" , req.cookies.token)
-        const user = await Account.findOne({token : req.cookies.token})
+        const user = await Account.findOne({token : req.cookies.token}).select('-password')
         // console.log(user)
         if(!user)         res.redirect(`${systemConfig.prefixAdmin}/auth/login`)
-        else              next();
+        else {
+            const role = await Role.findOne({
+                _id : user.role_id,
+                deleted : false,
+            }).select("title permissions")
+
+            res.locals.user = user;
+            res.locals.role = role;
+            next();
+        }
     }
 
 
