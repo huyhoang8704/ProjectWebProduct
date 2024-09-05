83b809afbc568c045f40d1e7268b253740090b2c
Nguyen Huy Hoang <137654109+huyhoang8704@users.noreply.github.com>
2024-09-05 09:28:13 +0700

    update

--- middlewares/admin/auth.middleware.js
+++ middlewares/admin/auth.middleware.js
@@ -13,6 +13,7 @@ module.exports.requireAuth = async (req , res, next) => {
         const user = await Account.findOne({token : req.cookies.token}).select('-password')
         if(!user)         res.redirect(`${systemConfig.prefixAdmin}/auth/login`)
         else {
+            // Khi đăng nhập xong thì trả về thông tin user và role để authorization
             const role = await Role.findOne({
                 _id : user.role_id,
                 deleted : false,
